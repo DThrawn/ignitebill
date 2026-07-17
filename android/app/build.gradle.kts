1113d62a7940cbc17655349ef6ea5dea5e5be341
DThrawn <darwin@example.com>
2026-07-17 15:52:27 +0200

    Fix CI: force SDK 34 for background service compatibility

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,23 +9,19 @@ plugins {
 
 android {
     namespace = "com.example.timer"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 34
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
-        // CORRECTION 1 : La bonne syntaxe Kotlin avec le signe "="
         isCoreLibraryDesugaringEnabled = true
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.ignitech.ignitebill"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        minSdk = 21
+        targetSdk = 34
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
