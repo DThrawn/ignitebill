7660be06b0aa54c364a0e4d6f753a7cc70580e41
DThrawn <darwin@example.com>
2026-07-18 09:45:46 +0200

    fix: final definitive build and release fix - unified environment and robust APK naming

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,14 +1,3 @@
-buildscript {
-    val kotlin_version = "2.4.10"
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
-    }
-}
-
 allprojects {
     repositories {
         google()
