39eeaad4faf9860f7c1d4fee171fa63568d0f46b
DThrawn <darwin@example.com>
2026-07-19 21:18:39 +0200

    Chore: Update AGP to 8.6.0 and Gradle to 8.7 as requested

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -24,7 +24,7 @@ pluginManagement {
 
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.5.2" apply false
+    id("com.android.application") version "8.6.0" apply false
     id("org.jetbrains.kotlin.android") version "2.0.21" apply false
     // Plugin pour la résolution automatique des toolchains Java (Foojay)
     // id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
